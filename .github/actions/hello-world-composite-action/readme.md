f1265462ebfd1ee636a58f7a767bc3158be1854a
Rob Foulkrod <rob foulkrod>
2022-12-11 21:30:34 -0500

    initial commit

---
+++ .github/actions/hello-world-composite-action/readme.md
@@ -0,0 +1,7 @@
+# Hello World Composite Action
+
+## Giving permission for shell script from windows
+
+```git
+  git update-index --chmod=+x goodbye.sh
+```
---
+++ .github/actions/js-one/index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+
+try {
+    // assume the best
+    core.setOutput("verified", true);
+    // `root-lab-path` input defined in action metadata file
+    const rootPath = core.getInput('root-lab-path');
+    console.log(`root-lab-path ${rootPath}`);
+    const dirContents = fs.readdirSync(rootPath);
+    console.log(`folders and files ${dirContents}!`);
+
+    //for each entry in folders
+    for (var i = 0; i < dirContents.length; i++) {
+        const path_string = rootPath + "/" + dirContents[i];
+        const isDirectory = fs.lstatSync(path_string).isDirectory();
+        console.log(`path_string ${path_string} | directory ${isDirectory}`);
+        const readmePath = path_string + "/README.md"
+        const readmeExists = fs.existsSync(readmePath);
+        console.log(`readme ${readmePath} | exists${readmeExists}`);
+        if (!readmeExists) {
+            core.setOutput("verified", false);
+            core.setFailed(`${path_string} does not have a README.md`);
+        }
+    }
+
+} catch (error) {
+    core.setFailed(error.message);
+}
