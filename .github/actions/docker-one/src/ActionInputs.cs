c0a262191b09f74c9fd292bc13a707d90e0a6c9d
Rob Foulkrod <rob foulkrod>
2022-12-13 16:18:40 -0500

    Omitting workspace input value

--- .github/actions/docker-one/src/ActionInputs.cs
+++ .github/actions/docker-one/src/ActionInputs.cs
@@ -42,11 +42,6 @@ public class ActionInputs
         HelpText = "The root directory to start recursive searching from.")]
     public string Directory { get; set; } = null!;
 
-    [Option('w', "workspace",
-        Required = true,
-        HelpText = "The workspace directory, or repository root directory.")]
-    public string WorkspaceDirectory { get; set; } = null!;
-
     static void ParseAndAssign(string? value, Action<string> assign)
     {
         if (value is { Length: > 0 } && assign is not null)
